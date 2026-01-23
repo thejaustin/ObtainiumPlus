42c2de04e64b0852c06fda975e74d6e8ebd5448b
thejaustin <the.j.austin@gmail.com>
2026-01-23 04:24:48 -0700

    Fix build configuration: Relax NDK version and Dart SDK constraints

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "dev.imranr.obtainium"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // 'flutter.ndkVersion' produces warnings (TODO can/should we switch back?)
+    // ndkVersion = "27.0.12077973" // 'flutter.ndkVersion' produces warnings (TODO can/should we switch back?)
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
