85abc910aa6d8928e7451fecbbd0e02f6cbb6658
thejaustin <the.j.austin@gmail.com>
2026-09-17 18:31:29 -0500

    fix: import AppConstants in app.dart (#320)
    Co-authored-by: Antigravity <antigravity@google.com>

--- lib/pages/app.dart
+++ lib/pages/app.dart
@@ -22,6 +22,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:obtainium/components/common/conditional_blur.dart';
 import 'package:obtainium/components/common/expressive_progress_indicator.dart';
+import 'package:obtainium/utils/app_constants.dart';
 import 'package:obtainium/utils/haptic_utils.dart';
 import 'package:obtainium/components/apps/app_version_history.dart';
 import 'package:obtainium/components/apps/app_changelog.dart';
